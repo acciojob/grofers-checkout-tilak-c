97835dafeab81061fae21f2627a8ff0ec2732c08
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 13:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const sumPrice=document.getElementsByClassName("price");
+let sum=0;
 const getSum = () => {
 //Add your code here
-  
+  for(let i=0;i<sumPrice.length;i++){
+	  sum+=parseInt(sumPrice[i].value);
+  }
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
